2ec792e955fe2ea8761909008eca251e2a184420
Nágella Nasser <nagellacnb@gmail.com>
2021-11-27 16:24:27 -0300

    Projeto base

---
+++ app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package fatec.padroesdeprojeto.io.marvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fatec.padroesdeprojeto.io.marvelapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
