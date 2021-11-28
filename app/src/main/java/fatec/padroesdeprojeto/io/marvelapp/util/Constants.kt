da27148db31efccd04b27acc0c10e06ffc1fbbf6
Nágella Nasser <nagellacnb@gmail.com>
2021-11-28 18:15:48 -0300

    Injeção de dependência - Criando a interface ServiceApi

---
+++ app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package fatec.padroesdeprojeto.io.marvelapp.util
+
+object Constants {
+    const val BASE_URL = " https://gateway.marvel.com/v1/public/"
+
+    const val PUBLIC_KEY = "8dfc1008d3f8ce950319681d54478806"
+    const val PRIVATE_KEY = "c0a35f37536e4da7dba2a8c5e61de0412e02c78c"
+}
