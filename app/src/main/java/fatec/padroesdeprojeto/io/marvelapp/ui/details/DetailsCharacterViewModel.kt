cd258aec83b2bc89b51d33a05a169b5253326a08
Nágella Nasser <nagellacnb@gmail.com>
2021-11-29 20:11:18 -0300

    Implementando o ViewModel Details

--- app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/ui/details/DetailsCharacterViewModel.kt
+++ app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/ui/details/DetailsCharacterViewModel.kt
@@ -1,6 +1,51 @@
 package fatec.padroesdeprojeto.io.marvelapp.ui.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import fatec.padroesdeprojeto.io.marvelapp.data.model.comic.ComicModelResponse
+import fatec.padroesdeprojeto.io.marvelapp.repository.MarvelRepository
+import fatec.padroesdeprojeto.io.marvelapp.ui.state.ResourceState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class DetailsCharacterViewModel: ViewModel() {
+@HiltViewModel
+class DetailsCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+) : ViewModel() {
+
+    private val _details =
+        MutableStateFlow<ResourceState<ComicModelResponse>>(ResourceState.Loading())
+    val details: StateFlow<ResourceState<ComicModelResponse>> = _details
+
+    fun fetch(characterId: Int) = viewModelScope.launch {
+        safeFetch(characterId)
+    }
+
+    private suspend fun safeFetch(characterId: Int) {
+        _details.value = ResourceState.Loading()
+        try {
+            val response = repository.getComics(characterId)
+            _details.value = handleResponse(response)
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> _details.value =
+                    ResourceState.Error("Erro de conexão com a internet")
+                else -> _details.value = ResourceState.Error("Falha na conversão de dados")
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<ComicModelResponse>): ResourceState<ComicModelResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { values ->
+                return ResourceState.Success(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
 }
