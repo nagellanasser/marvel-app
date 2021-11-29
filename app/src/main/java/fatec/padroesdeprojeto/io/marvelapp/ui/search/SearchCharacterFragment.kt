08f69d378968cdbfb6ec576a4d4fdef71388b004
Nágella Nasser <nagellacnb@gmail.com>
2021-11-29 19:10:43 -0300

    Implementando o Fragment de Pesquisa - SearchCharacter

--- app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/ui/search/SearchCharacterFragment.kt
+++ app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/ui/search/SearchCharacterFragment.kt
@@ -1,17 +1,128 @@
 package fatec.padroesdeprojeto.io.marvelapp.ui.search
 
+import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import fatec.padroesdeprojeto.io.marvelapp.R
 import fatec.padroesdeprojeto.io.marvelapp.databinding.FragmentSearchCharacterBinding
+import fatec.padroesdeprojeto.io.marvelapp.ui.adapters.CharacterAdapter
 import fatec.padroesdeprojeto.io.marvelapp.ui.base.BaseFragment
+import fatec.padroesdeprojeto.io.marvelapp.ui.list.ListCharacterFragmentDirections
+import fatec.padroesdeprojeto.io.marvelapp.ui.state.ResourceState
+import fatec.padroesdeprojeto.io.marvelapp.util.Constants.DEFAULT_QUERY
+import fatec.padroesdeprojeto.io.marvelapp.util.Constants.LAST_SEARCH_QUERY
+import fatec.padroesdeprojeto.io.marvelapp.util.hide
+import fatec.padroesdeprojeto.io.marvelapp.util.show
+import fatec.padroesdeprojeto.io.marvelapp.util.toast
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SearchCharacterFragment :
     BaseFragment<FragmentSearchCharacterBinding, SearchCharacterViewModel>() {
 
     override val viewModel: SearchCharacterViewModel by viewModels()
+    private val characterAdapter by lazy { CharacterAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecycleView()
+        clickAdapter()
+
+        val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
+
+        searchInit(query)
+        collectObserver()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.searchCharacter.collect {result ->
+            when(result){
+                is ResourceState.Success -> {
+                    binding.progressbarSearch.hide()
+                    result.data?.let {
+                        characterAdapter.characters = it.data.results.toList()
+                    }
+                }
+                is ResourceState.Error -> {
+                    binding.progressbarSearch.hide()
+                    result.message?.let { message ->
+                        toast(getString(R.string.an_error_occurred))
+                        Timber.tag("SearchCharacterFragment").e("Error -> $message")
+                    }
+                }
+                is ResourceState.Loading -> {
+                    binding.progressbarSearch.show()
+                }
+                else -> {}
+            }
+        }
+    }
+
+    private fun searchInit(query: String) = with(binding) {
+        edSearchCharacter.setText(query)
+        edSearchCharacter.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                updateCharacterList()
+                true
+            } else {
+                false
+            }
+        }
+
+        edSearchCharacter.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                updateCharacterList()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun updateCharacterList() = with(binding) {
+        edSearchCharacter.editableText.trim().let {
+            if (it.isNotEmpty()) {
+                searchQuery(it.toString())
+            }
+        }
+    }
+
+    private fun searchQuery(query: String) {
+        viewModel.fetch(query)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(
+            LAST_SEARCH_QUERY,
+            binding.edSearchCharacter.editableText.trim().toString()
+        )
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnClickListener { characterModel ->
+            val action = SearchCharacterFragmentDirections
+                .actionSearchCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupRecycleView() = with(binding) {
+        rvSearchCharacter.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
@@ -19,3 +130,6 @@ class SearchCharacterFragment :
     ): FragmentSearchCharacterBinding =
         FragmentSearchCharacterBinding.inflate(inflater, container, false)
 }
+
+
+
--- app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/util/Constants.kt
+++ app/src/main/java/fatec/padroesdeprojeto/io/marvelapp/util/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val PUBLIC_KEY = "8dfc1008d3f8ce950319681d54478806"
     const val PRIVATE_KEY = "c0a35f37536e4da7dba2a8c5e61de0412e02c78c"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
